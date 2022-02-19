03d6ded534c30481181228a480db270909010066
ljx360 <79862310+ljx360@users.noreply.github.com>
2022-02-19 09:05:57 +0800

    Update ljx.sh

--- ljx.sh
+++ ljx.sh
@@ -1,2 +1,9 @@
-curl https://raw.githubusercontent.com/ljx360/homelede_2022/main/homelede.config -o .config
+#/bin/sh
+
+$url="https://raw.githubusercontent.com/ljx360/homelede_2022/main/homelede.config"
+$out=".config"
+echo -e "curl get $url"
+curl $url -o $out
+
+echo -e "list dir"
 ls -lsa
